db892e70d96f97647e2f46a5dca7c1bf738ab556
楓太 小山 <190138@jc-21.jp>
2021-12-03 11:19:00 +0900

    タブ

--- App3/page/MainPage.xaml.cs
+++ App3/page/MainPage.xaml.cs
@@ -1,6 +1,11 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace NavPageSample
 {
--- App3/page/MainPage1.xaml.cs
+++ App3/page/MainPage1.xaml.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace NavPageSample
 {
