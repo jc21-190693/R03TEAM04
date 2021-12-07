4b35d09743a4a43986e33c89d1a02f4989531295
楓太 小山 <190138@jc-21.jp>
2021-12-07 15:57:32 +0900

    タブ

---
+++ App3/page/MainPage3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavPageSample.page
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage3 : ContentPage
+    {
+        public MainPage3()
+        {
+            InitializeComponent();
+        }
+    }
+}
