eeef9154f6cafd0cce94a17faf50f6b19f16bb0e
遥香 鈴木 <190455@jc-21.jp>
2021-12-07 14:44:43 +0900

    てｓｔ

--- App3/page/MainPage.xaml.cs
+++ App3/page/MainPage.xaml.cs
@@ -1,6 +1,13 @@
 ﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
 
 namespace App3
 {
@@ -10,7 +17,23 @@ namespace App3
         public MainPage()
         {
             InitializeComponent();
+            // 上部のバーを消す
+            // 消したいページ毎に実行する必要がある
+            NavigationPage.SetHasNavigationBar(this, false);
         }
-    }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // SubPageに遷移する
+            Navigation.PushAsync(new SubPage());
 
+            // このようにMainPageプロパティに遷移したいページを渡すことで画面を切り替えることができる
+            // この場合NavigationPageのように遷移したページをスタックしないので、前のページに戻る処理などは自前で管理する必要がある
+            //App.Current.MainPage = new SubPage();
+        }
+
+
+
+
+    }
 }
+
