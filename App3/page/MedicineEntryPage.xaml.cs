97733727cd9a8443a3baa14d028d1a9abed37d56
将 佐藤 <190663@jc-21.jp>
2021-12-22 12:41:56 +0900

    アンケート画面下空白削除他

---
+++ App3/page/MedicineEntryPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavPageSample.page
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MedicineEntryPage : ContentPage
+    {
+        public MedicineEntryPage()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
